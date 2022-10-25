f4a38845a4e99d9c1d78d6892230bb87f3ac5427
AnastesiaSpirido <spiridonastja@gmail.com>
2022-10-25 18:00:04 +0300

    first home work

---
+++ ДымоходРу.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+        <title> Дымоход </title>
+
+    </head>
+<body>
+  
+    <header class="header">
+         <div class="container_top">
+            <div class="header_inner">
+                  <div class="header_logo">
+                    <img src="images/Logo.jpg" alt="">
+                 </div>
+            <div class="menu">
+                <a href="#">
+                    <img src="images/social/menu.png" alt="">
+                </a>
+            </div>
+                 <nav class="nav">
+                    <a class="nav_link" href="#">каталог товаров</a>
+                    <a class="nav_link" href="#">доставка и оплата</a>
+                    <a class="nav_link" href="#">цены</a>
+                    <a class="nav_link" href="#">контакты</a>
+                </nav>
+                <div class="number"> +7 (812) 
+                    <span>509 21  98</span>
+                </div>
+                <a href="#" class="call"> <img src="images/social/contact.png" alt=""> </a>
+                <div class="social">
+                    <a  href="#"><img src="images/social/vk.png" alt=""></a>
+                    <a  href="#"><img src="images/social/tg.png" alt=""></a>
+                    <a  href="#"><img src="images/social/ig.png" alt=""></a>
+                    <a  href="#"><img src="images/social/wa.png" alt=""></a>
+                </div>
+                <div class="img">
+                    <a  href="#"><img src="images/social/search.png" alt=""></a>
+                    <a  href="#"><img src="images/social/heart.png" alt=""></a>
+                    <a  href="#"><img src="images/social/bascket.png" alt=""></a>
+                </div>
+            </div>
+         </div>
+
+    </header>
+    
+    <h1 class="h1"> Монтаж дымоходов</h1>
+    <section class="container">
+        <div class="section">
+       <div class="home">
+          <p>Главная</p>
+          <img src="images/social/nav.png" alt="">
+          <a href="#">Монтаж дымоходов</a>
+        </div>
+    </div>
+    </section>
+
+    <section class="container">
+          <div class="section">
+            <p class="infa">В <a href="#" class="thref"> Дымоход.ру </a>  вам помогут как приобрести, так и установить дымоход любой конфигурации. 
+                В нашем монтажном отделе, состоящим из трёх бригад работают квалифицированные специалисты, 
+                которые ежегодно проходят курсы повышения квалификации. 
+                Все работы проводим качественно и аккуратно, в короткий срок и на совесть .</p>
+          </div>
+    </section>
+
+    <section class="container">
+         <div class="block">
+            <div class="flex_container">
+            <div class="first">
+               <div class="lorem">Lorem ipsum dolor sit amet
+                <span>lorem ipsum</span> </div>
+                <div class="list">    
+                   <ul type="disc">
+                        <li><span>Доступные цены</span></li>
+                        <li><span>Собственное производсвто Дымоходов</span></li>
+                        <li><span>Выполнение необходимых работ, связанных с установкой
+                             камина, печи, котла</span></li>
+                        <li><span>Гарантия (от трёх до десяти лет)</span></li>
+                        <li><span>Работаем чисто, аккуратно, убираем за собой, не сквернословим</span></li>
+                        <li><span>Только русские квалифицированные специалисты</span></li>
+                        <li><span>Устанавливаем любые дымоходы всех производителей</span></li>
+                    </ul>
+                </div>
+            </div>
+         <div class="second">
+            <div class="ipsum">
+                <img class="blob" src="images/Blob.png" alt="">
+            </div>
+        </div>
+      </div>
+     </div>
+    </section>
+
+    <section class="container">
+        <div class="section">
+            <p class="title">
+                Процесс выполнения монтажа
+            </p> 
+            <div class="flex_container_process">
+               <div class="process first_1">
+                <ul>
+                  <li><p><img src="images/num/1.png" alt="">Рассчёт предварительной заявки</p></li> 
+                  <li><p> <img src="images/num/2.png" alt="">Замер объекта нашим инженером</p></li>
+                  <li><p><img src="images/num/3.png" alt="">Составление Коммерческого предложения 
+                    по изделиям дымохода и окончательной сметы на  работы </p></li>
+                </ul>
+             </div>
+
+         <div class="process second_1">
+            <ul>
+                <li><p><img src="images/num/4.png" alt=""> Изготовление Дымохода, покраска (если требуется)</p></li>
+                <li><p><img src="images/num/5.png" alt="">Монтаж продукции, проверка и наладкапробный запуск системы и последующая эксплуатация</p></li>
+                <li><p><img src="images/num/6.png" alt="">Гарантийное обслуживание по соответствующем сроку</p></li>
+            </ul>
+        </div>
+            
+
+            </div>
+        </div>
+    </section>
+
+    <section class="container">
+        <div class="section">
+            <p class="title why">
+                Почему Дымоход.ру?
+            </p> 
+            <div class="infa_1">
+            <span>В отличие от других производителей, 
+                мы ручаемся за качество продукции, т.к. все 
+                Дымоходы изготавливаются из качественной стали, 
+                проверенного теплоизолятора, 
+                по современной технологии плазменной сварки «встык» </span>
+            </div>
+            </div>
+    </section>
+
+<section class="container">
+    <div class="section">
+    <div class="infa_2">
+        <article> <img src="images/guarantee 1.svg" alt="">
+        <span class="title_infa_2"> Большой гарантийный срок</span>
+        <p class="text_infa_2"> Крепёж, стыковка элементов – <span>10 лет</span> <br>
+            Сквозная коррозия труб – <span> 5 лет</span><br>
+            Гидроизоляция – <span>3 года</span><br>
+            </p>
+        </article>
+        
+        <article> <img src="images/architect 1.svg" alt="">
+        <span class="title_infa_2">Помощь в покупке агреата</span>
+        <p class="text_infa_2"> (печи, камина, котла), а также качественной <br> установке
+             на объекте с последующим <br> гарантийным сервисом</p>
+        </article>
+        
+        <article> <img src="images/review 1.svg" alt="">
+        <span class="title_infa_2"> Реальные отзывы клиентов</span>
+        <p class="text_infa_2"> Убедитесь в добросовестности исполнения, 
+            прочитав Отзывы клиентов в специальном  разделе.</p>
+        </article>
+        </div>
+
+    </div>
+ </section>
+
+<section class="container">
+    <div class="section">
+        <div class="help">
+            <span>необходима консультация? <p>Оставьте заявку
+                или свяжитесь с нами в социальных сетях
+               </p>
+            </span>
+            <div class="social_2">
+                <a  href="#"><img src="images/social/vkontakte.png" alt=""></a>
+                <a  href="#"><img src="images/social/telegram.png" alt=""></a>
+                <a  href="#"><img src="images/social/instagram.png" alt=""></a>
+                <a  href="#"><img src="images/social/whatsapp.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="container">
+    <div class="section">
+        <div class="forma">
+            <div class="text_forma">
+                Для <span> консультации </span>заполните форму! <p>
+                    С Вами свяжутся в ближайшее время
+               </p>
+            </div>
+
+            <form action="ДымоходРу.php" method="post" target="_blank">
+                <input type="text" class="pole" name="text" placeholder="Ваше имя">
+				<input type="tel" class="pole" name="tel"  placeholder="Ваш телефон"></input>
+				<input type="submit" class="otpravit" name="sab" value="отправить">
+            </form>
+
+            <?php
+$urok="Дфмоход: ";
+error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+// создание переменных из полей формы		
+if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
+if (isset($_POST['tel']))		{$tel		= $_POST['tel'];		if ($tel == '')	{unset($tel);}}
+if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		if ($sab == '')		{unset($sab);}}
+//стирание треугольных скобок из полей формы
+/* комментарий */
+if (isset($text) ) {
+    $text=stripslashes($text);
+    $text=htmlspecialchars($text);
+    }
+
+if (isset($tel) ) {
+$tel=stripslashes($tel);
+$tel=htmlspecialchars($tel);
+}
+// адрес почты куда придет письмо
+$address="spiridonastja@gmail.com";
+// текст письма 
+$note_text="Тема : $urok\r\n Имя : $text \r\n Телефон : $tel";
+
+if (isset ($sab) ) {
+mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
+// сообщение после отправки формы
+    
+echo "<p  style='color:white;font-weight:bold'> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b style='color:red;'> $tel</b> </p>";
+}
+
+?>
+
+        </div>
+    </div>
+</section>
+
+<section class="container">
+    <div class="section">
+        <div class="text_video">
+            <p class="title">Видео об установке</p>
+            <div class="infa_3">
+                <span class="infa_video">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor
+                     in reprehenderit in voluptate velit esse cillum
+                      dolore eu fugiat nulla pariatur. Excepteur
+                       sint occaecat cupidatat non proident, sunt 
+                       in culpa qui officia
+                     deserunt mollit anim id est laborum. </span>
+                    </div>
+         </div>
+
+         <div class="image_stack">
+            <div class="image_item image_item_top">
+              <img src="images/video/vidos.png" alt="">
+            </div>
+         </div>
+    </div>
+</section>
+
+<section class="container">
+    <div class="section">
+        <div class="raboty_text">
+            <p class="title">Фото выполненных работ</p>
+              <div class="infa_3">
+                <span class="infa_video">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor
+                     in reprehenderit in voluptate velit esse cillum
+                      dolore eu fugiat nulla pariatur. Excepteur
+                       sint occaecat cupidatat non proident, sunt 
+                       in culpa qui officia
+                     deserunt mollit anim id est laborum. </span>
+                    </div>
+        </div>
+        <div class="raboty_foto">
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-1.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-2.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-3.png" alt="">
+            </div>
+
+        </div>
+        <div class="raboty_foto">
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-4.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-5.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-6.png" alt="">
+            </div>
+
+            <div class="raboty_item">
+                <img class="raboty_img" src="images/raboty/img-7.png" alt="">
+            </div>
+         </div>
+    </div>
+</section>
+
+
+<footer class="foot">
+    <div class="section clearfix middle4-article">
+    <article>
+    <h2> Каталог </h2>
+    <div class="foot_text"> </div>
+    <p> <a href="#"> Дымоходы</a>
+    <a href="#"> Дымососы </a> 
+    <a href="#"> Печи для дома</a> 
+    <a href="#"> Печи для бани </a>
+     </p>
+     <p class="two_block"> <a href="#"> Камины</a>
+        <a href="#"> Силикат кальция </a> 
+        <a href="#"> Котлы</a> 
+        <a href="#"> Ещё</a>
+         </p>
+    </article>
+    <article>
+    <h2> Информация </h2>
+    <div class="foot_text"> </div>
+    <p> <a href="#"> Каталог</a>
+    <a href="#"> Установка и монтаж </a> 
+    <a href="#"> Отзывы</a> 
+    <a href="#"> Ликвидация товара </a>
+    <p class="two_block">
+     <a href="#"> Полезное </a>
+     <a href="#"> Контакты</a>
+    <a href="#"> Оплата и доставка</a> 
+    </p>
+     </p>
+    </article>
+    <div class="foot_text"> 
+    <article class="time">
+    <h2> Время работы </h2>
+    <div class="last">
+    <p class="text_last">
+        <img src="images/social/clock.png" alt=""> <span>пн-пт</span>
+        9:00-20:00 <span>сб-вс </span>10:00-18:00
+     </p>
+     <p class="text_last">
+        <img src="images/social/karta.png" alt="">
+        г. Санкт - Петербург ,ул. Тамбасова д.7
+     </p>
+
+     <div class="social_3">
+        <a  href="#"><img src="images/social/vk.png" alt=""></a>
+        <a  href="#"><img src="images/social/tg.png" alt=""></a>
+        <a  href="#"><img src="images/social/ig.png" alt=""></a>
+        <a  href="#"><img src="images/social/wa.png" alt=""></a>
+    </div>
+</div>
+    </div>
+    </article>
+    </div>
+    
+    </footer>
+
+<script>
+var videoA = function () {this.innerHTML = '<iframe width="771" height="438" src="https://www.youtube.com/embed/7CMa4vr1Sa4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'}; 
+    document.querySelector('.image_item').addEventListener('click', function (e) { this.removeEventListener('click', videoA, false); videoA.apply(this, arguments); } , false);
+</script>
+</body>
+</html>
